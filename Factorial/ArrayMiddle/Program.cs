dab78ec24b1b821b8b432a2ee064332846c63b63
DebugSlam <batkovan@yahoo.com>
2023-11-02 21:52:50 +0200

    Exam prep I

---
+++ Factorial/ArrayMiddle/Program.cs
@@ -0,0 +1,9 @@
+﻿
+List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+int middleDigit = (numbers.Count -1) / 2;
+int neibDigit = middleDigit + 1;
+
+double result = (numbers[middleDigit] + numbers[neibDigit]) / 2.0;
+
+Console.WriteLine($"{result:F2}");
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FactorelNum(num1));
+
+static int FactorelNum(int n1)
+{
+    int sumDigit = 1;
+    int Amount = 0;
+    int digit = 0;
+
+    while (n1>0)
+    {
+        digit = n1 % 10;
+        n1 = n1 / 10;
+
+        if (digit % 2 == 0)
+        {
+            for (int i = 1; i <= digit; i++)
+            {
+                sumDigit *= i;
+            }
+            Amount += sumDigit;
+        }
+
+        sumDigit = 1;
+    }
+    return Amount;
+}
