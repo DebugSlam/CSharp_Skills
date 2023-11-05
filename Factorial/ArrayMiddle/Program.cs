c90a0f52be5b692f08a00811ee6b3c23e6aad107
DebugSlam <batkovan@yahoo.com>
2023-11-05 17:52:49 +0200

    revert

--- Factorial/ArrayMiddle/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-
-int middleDigit = (numbers.Count -1) / 2;
-int neibDigit = middleDigit + 1;
-
-double result = (numbers[middleDigit] + numbers[neibDigit]) / 2.0;
-
-Console.WriteLine($"{result:F2}");
